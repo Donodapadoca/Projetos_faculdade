f366f5b74d0b2bcd01ce146e0fa984fd0d4c2561
João  Pedro <62611497+Donodapadoca@users.noreply.github.com>
2023-06-21 17:57:08 -0300

    Cedulas (falta terminar)
    não consegui fazer com que as celulas que "não existem sejam imprimidas" RESOLVER ISSO!!!

---
+++ cedulas.py
@@ -0,0 +1,110 @@
+valor = float(input("Digite o valor: "))
+
+
+if valor>100:
+    x=valor/100
+
+    new_x = int(x)
+
+    valor= valor-(new_x*100)
+    print("Tem exatamente: ", new_x,"notas de 100")
+
+else:
+      print("Tem exatamente 0 notas de 100")
+
+if valor<1:
+        exit()
+
+if valor>50:
+    y=valor/50
+
+    new_y = int(y)
+
+    valor = valor-(new_y*50)
+    print("Tem exatamente: ", new_y,"notas de 50")
+    
+else:
+      print("Tem exatamente 0 notas de 50")
+
+if valor<1:
+        exit()
+    
+
+if valor>20:
+    z = valor/20
+    
+    new_z = int(z)
+
+    valor = valor-(new_z*20)
+    print("Tem exatamente: ", new_z,"notas de 20")
+
+    
+else:
+      print("Tem exatamente 0 notas de 20")
+
+if valor<1:
+        exit()
+
+if valor>10:
+    k = valor/10
+    
+    new_k = int(k)
+
+    valor = valor-(new_k*10)
+    print("Tem exatamente: ", new_k,"notas de 10")
+    
+else:
+      print("Tem exatamente 0 notas de 10")
+
+if valor<1:
+        exit()
+
+if valor>5:
+    l =valor/5
+
+    new_l = int(l)
+
+    valor = valor-(new_l*5)
+    print("Tem exatamente: ", new_l,"notas de 5")
+    
+else:
+      print("Tem exatamente 0 notas de 5")
+
+if valor<1:
+        exit()
+
+if valor>2:
+    m=valor/2
+
+    new_m = int(m)
+
+    valor = valor-(new_m*2)
+    print("Tem exatamente: ", new_m,"notas de 2")
+    
+else:
+      print("Tem exatamente 0 notas de 2")
+     
+if valor<1:
+        exit()
+
+
+if valor>1:
+    n=valor/1
+
+    new_n = int(n)
+    print("Tem exatamente: ", new_n,"notas de 1")
+    
+else:
+      print("Tem exatamente 0 notas de 1")
+
+if valor<1:
+        exit()
+
+
+   
+    
+    
+    
+    
+    
+
